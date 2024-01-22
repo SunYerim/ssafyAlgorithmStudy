f18d58d3fb7a8c0594ccedc0a9f54c38e66b3422
baejun <sonqowns2@naver.com>
2024-01-22 15:40:30 +0900

    baejun test

---
+++ baejun/BaejunRepo/src/Algorithm/BAEKJOON/BOJ245483/BOJ245483.java
@@ -0,0 +1,10 @@
+package Algorithm.BAEKJOON.BOJ245483;
+
+public class BOJ245483 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("해윙~");
+	}
+
+}
