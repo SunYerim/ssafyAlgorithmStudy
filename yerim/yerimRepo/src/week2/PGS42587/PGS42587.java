765541fa0a637f075213212804f1debdc6559a4a
yerimya <syl1602@naver.com>
2024-01-30 01:17:11 +0900

    [PGS42587] 프로세스

---
+++ yerim/yerimRepo/src/week2/PGS42587/PGS42587.java
@@ -0,0 +1,55 @@
+package week2.PGS42587;
+
+import java.util.PriorityQueue;
+import java.util.Collections;
+import java.util.Queue;
+import java.util.LinkedList;
+
+// 우선순위 큐
+public class PGS42587 {
+    public int solution(int[] priorities, int location) {
+
+        // Node 형 우선순위 큐 선언
+        PriorityQueue<Node> priorityQueue = new PriorityQueue<>();
+        Queue<Node> queue = new LinkedList<>();
+
+        for (int i = 0; i < priorities.length; i++) {
+            Node node = new Node(i, priorities[i]);
+            priorityQueue.add(node);
+            queue.add(node);
+        }
+
+        int answer = 0;
+        while (!queue.isEmpty()) {
+            Node current = queue.poll();
+            if (current.priority == priorityQueue.peek().priority) {
+                priorityQueue.poll();
+                answer++;
+                if (current.position == location) {
+                    break;
+                }
+            } else {
+                queue.add(current);
+            }
+        }
+        return answer;
+    }
+
+
+
+    public class Node implements Comparable<Node> {
+        int position;
+        int priority;
+
+        public Node(int position, int priority) {
+            this.position = position;
+            this.priority = priority;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return o.priority - this.priority;
+        }
+    }
+
+}
