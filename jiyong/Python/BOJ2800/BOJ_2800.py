e1b6b8f0bb5964fc40bf54e7729e40aab1a3d721
DaftenP <bjeeyong99@gmail.com>
2024-01-31 17:46:24 +0900

    docs: 1주차 문제 풀이 및 목록 갱신

--- jiyong/Python/BOJ2800/BOJ_2800.py
+++ jiyong/Python/BOJ2800/BOJ_2800.py
@@ -1,14 +1,16 @@
 import sys
 import copy
 
-expr = [*sys.stdin.readline().rstrip()]
-br_count = expr.count('(')
+# input data: (2+(2*2)+2)
+
+expr = [*sys.stdin.readline().rstrip()]  # ['(', '2', '+', '(', '2', '*', '2', ')', '+', '2', ')']
+br_count = expr.count('(')  # 2
 answer = set()
 
-for comb in range(1, 2 ** br_count):
+for comb in range(1, 2 ** br_count):  # comb : 1 ~ 4 -> 0b001 ~ 0b100
     stack = []
-    temp = copy.deepcopy(expr)
-    for i in range(len(temp)):
+    temp = copy.deepcopy(expr)  # ['(', '2', '+', '(', '2', '*', '2', ')', '+', '2', ')']
+    for i in range(len(temp)):  # i : 0 ~ 11
         if temp[i] == '(':
             if comb & 1:
                 stack.append('{')
@@ -17,8 +19,16 @@ for comb in range(1, 2 ** br_count):
                 stack.append('(')
             comb >>= 1
         elif temp[i] == ')':
-            if stack[-1] == '{': temp[i] = ''
+            if stack[-1] == '{':
+                temp[i] = ''
             stack.pop()
     answer.add(''.join(temp))
 
 print(*sorted(answer), sep='\n')
+
+'''
+output :
+(2+2*2+2)
+2+(2*2)+2
+2+2*2+2
+'''
