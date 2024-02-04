0f83fdfd21215bfde385b214915dc849ffa19ed0
yerimya <syl1602@naver.com>
2024-02-05 01:09:48 +0900

    [PGS42577] 전화번호 목록 - 코드

---
+++ yerim/yerimRepo/src/week3/PGS42577/PGS42577.java
@@ -0,0 +1,24 @@
+package week3.PGS42577;
+
+import java.util.*;
+public class PGS42577 {
+    public boolean solution(String[] phone_book) {
+
+        Map<String, Integer> phoneMap = new HashMap<>();
+
+        for (int i = 0; i < phone_book.length; i++) {
+            phoneMap.put(phone_book[i], i);
+        }
+
+        for (int i = 0; i < phone_book.length; i++) {
+            for (int j = 0; j < phone_book[i].length(); j++) {
+                if (phoneMap.containsKey(phone_book[i].substring(0, j))){
+                    return false;
+                }
+            }
+        }
+        return true;
+
+
+    }
+}
