e435afbda64b3161777275ab8e93f5544555e9c4
DaftenP <bjeeyong99@gmail.com>
2024-02-07 17:56:22 +0900

    feat: BOJ 1060 temp

--- jiyong/Java/src/BOJ1060/Main.java
+++ jiyong/Java/src/BOJ1060/Main.java
@@ -1,9 +1,7 @@
 package BOJ1060;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
