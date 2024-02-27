e8ac2a5db23813738072a769eee0af73bf6607ea
jiyong <bjeeyong99@gmail.com>
2024-02-27 11:29:19 +0900

    feat: SWEA 4008 숫자만들기 (AC)

--- capacityTest/0225_숫자만들기/SWEA_4008_jiyong.py
+++ capacityTest/0225_숫자만들기/SWEA_4008_jiyong.py
@@ -1,31 +1,43 @@
-from itertools import product
-
 T = int(input())
 
+min_val = int(1e8)
+max_val = -int(1e8)
 
-def operate(operand, operator):
-    result = operand[0]
+
+def operate(ops):
+    global min_val, max_val
+    result = operands[0]
     for i in range(N - 1):
-        if operator[i] == '+':
-            result += operand[i + 1]
-        elif operator[i] == '-':
-            result -= operand[i + 1]
-        elif operator[i] == '*':
-            result *= operand[i + 1]
+        if ops[i] == '+':
+            result += operands[i + 1]
+        elif ops[i] == '-':
+            result -= operands[i + 1]
+        elif ops[i] == '*':
+            result *= operands[i + 1]
         else:
-            result = int(result / operand[i + 1])
-    return result
+            result = int(result / operands[i + 1])
+    min_val = min(min_val, result)
+    max_val = max(max_val, result)
+
+
+def product(cnt):
+    if cnt == N - 1:
+        operate(operator)
+    for i, op in enumerate(['+', '-', '*', '/']):
+        if operator_count[i] == 0:
+            continue
+        operator[cnt] = op
+        operator_count[i] -= 1
+        product(cnt + 1)
+        operator_count[i] += 1
 
 
 for tc in range(1, T + 1):
     N = int(input())
     operator_count = list(map(int, input().split()))
+    operator = [None] * (N - 1)
     operands = list(map(int, input().split()))
-    results = []
-    operator = [None] * N
-    for ops in product(['+', '-', '*', '/'], repeat=N - 1):
-        if ops.count('+') <= operator_count[0] and ops.count('-') <= operator_count[1] and ops.count('*') <= \
-                operator_count[2] and ops.count('/') <= operator_count[3]:
-            results.append(operate(operands, ops))
-    results.sort()
-    print(f'#{tc} {results[-1] - results[0]}')
+    min_val = int(1e8)
+    max_val = -int(1e8)
+    product(0)
+    print(f'#{tc} {max_val - min_val}')
