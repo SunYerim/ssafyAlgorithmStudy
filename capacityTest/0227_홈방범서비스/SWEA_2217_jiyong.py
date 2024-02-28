1d72260874bd9b88181f109dc90f2b2945809b6f
jiyong <bjeeyong99@gmail.com>
2024-02-28 12:23:10 +0900

    feat: SWEA 2217 홈방범서비스 (AC)

---
+++ capacityTest/0227_홈방범서비스/SWEA_2217_jiyong.py
@@ -0,0 +1,27 @@
+T = int(input())
+
+
+def get_distance(r1, c1, r2, c2):
+    return abs(r1 - r2) + abs(c1 - c2)
+
+
+for tc in range(1, T + 1):
+    N, M = map(int, input().split())
+    houses = []
+    for r in range(N):
+        for c, h in enumerate(map(int, input().split())):
+            if h == 1:
+                houses.append((r, c))
+    sum_map = [[[0] * (2 * N) for _ in range(N)] for __ in range(N)]
+    for r in range(N):
+        for c in range(N):
+            for house in houses:
+                sum_map[r][c][get_distance(r, c, *house) + 1] += 1
+    answer = 0
+    for r in range(N):
+        for c in range(N):
+            for k in range(1, 2 * N):
+                sum_map[r][c][k] += sum_map[r][c][k - 1]
+                if 0 <= sum_map[r][c][k] * M - (k ** 2 + (k - 1) ** 2):
+                    answer = max(answer, sum_map[r][c][k])
+    print(f'#{tc} {answer}')
