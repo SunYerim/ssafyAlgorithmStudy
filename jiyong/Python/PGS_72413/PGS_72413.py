399bb037036b60a14c8b6ebad931c5b3e5707260
jiyong <bjeeyong99@gmail.com>
2024-03-11 14:22:01 +0900

    feat: PGS 72413 (AC)

--- jiyong/Python/PGS_72413/PGS_72413.py
+++ jiyong/Python/PGS_72413/PGS_72413.py
@@ -11,8 +11,4 @@ def solution(n, s, a, b, fares):
         for r in range(1, n + 1):
             for c in range(1, n + 1):
                 dist_table[r][c] = min(dist_table[r][k] + dist_table[k][c], dist_table[r][c])
-    print(*dist_table, sep='\n')
     return min([dist_table[x][a] + dist_table[x][b] + dist_table[x][s] for x in range(1, n + 1)])
-
-
-print(solution(5, 4, 5, 1, [[1, 2, 1], [1, 3, 1], [1, 4, 5], [2, 3, 2], [3, 4, 2], [2, 5, 1], [3, 5, 2], [4, 5, 5]]))
