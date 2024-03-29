ad9b87ce77166285535d5d7c8068424280a131d4
jiyong <bjeeyong99@gmail.com>
2024-03-29 17:55:38 +0900

    feat: BOJ 1787 (AC)

---
+++ jiyong/Python/Week10/BOJ1787/BOJ_1787.py
@@ -0,0 +1,28 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+S = input().rstrip()
+pi = [0] * n
+answer = 0
+lp, rp = 0, 1
+
+while rp < n:
+    if S[lp] == S[rp]:
+        lp += 1
+        pi[rp] = lp
+        rp += 1
+    else:
+        if lp != 0:
+            lp = pi[lp - 1]
+        else:
+            pi[rp] = 0
+            rp += 1
+
+for i in range(n):
+    if pi[i]:
+        if pi[pi[i] - 1]:
+            pi[i] = pi[pi[i] - 1]
+        answer += i - pi[i]
+print(answer)
