59fb78a0789f39065ef0818af1ccc1ebb88e402d
sonbaejun <sonqowns2@naver.com>
2024-04-10 21:19:32 +0900

    feat : [공통][레벨3] 프로그래머스 여행경로

--- baejun/BaejunRepo/src/week11/PGM43164/PGM43164.java
+++ baejun/BaejunRepo/src/week11/PGM43164/PGM43164.java
@@ -1,62 +1,37 @@
 package week11.PGM43164;
 
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.PriorityQueue;
-
-
-public class PGM43164 {
-	public static void main(String[] args) throws IOException {
-		Solution solution = new Solution();
-		String[][] tickets = {{"ICN", "AAA"}, {"ICN", "CCC"}, {"CCC", "DDD"}, {"AAA", "BBB"}, {"AAA", "BBB"}, {"DDD", "ICN"}, {"BBB", "AAA"}};
-		solution.solution(tickets);
-	}
-}
+import java.util.*;
 
 class Solution {
-	static Map<String, PriorityQueue<String>> map;
-
-	public String[] solution(String[][] tickets) {
-		String[] answer = new String[tickets.length+1];
-		int answerIdx = -1;
-
-		map = new HashMap<>();
-		for(int i = 0; i < tickets.length; i++) {
-			if(map.get(tickets[i][0]) == null) { // 없음 생성
-				map.put(tickets[i][0], new PriorityQueue<>());
-				map.get(tickets[i][0]).add(tickets[i][1]);
-			} else { // 있음 queue에 value 추가
-				map.get(tickets[i][0]).add(tickets[i][1]);
-			}
-		}
-
-		String value = "ICN";
-		String waiting = "";
-		boolean flag = false;
-		answer[++answerIdx] = value;
-
-		while(!map.get(value).isEmpty()) {
-			answer[++answerIdx] = map.get(value).poll();
-			if(map.get(answer[answerIdx]) != null && !map.get(answer[answerIdx]).isEmpty()) {
-				value = answer[answerIdx];
-			} else {
-				if(!flag) {
-					flag = true;
-					waiting = (answer[answerIdx]);
-					answerIdx--;
-				}
-			}
-		}
-		value = waiting;
-		while(map.get(waiting) != null && !map.get(waiting).isEmpty()) {
-			answer[++answerIdx] = map.get(waiting).poll();
-			if(map.get(answer[answerIdx]) != null && !map.get(answer[answerIdx]).isEmpty()) {
-				value = answer[answerIdx];
-			}
-		}
-		System.out.println(Arrays.toString(answer));
-		return answer;
-	}
+    boolean[] visited;
+    ArrayList<String> allRoute; 
+    
+    public String[] solution(String[][] tickets) {
+        String[] answer = {};
+        int cnt = 0;
+        visited = new boolean[tickets.length];
+        allRoute = new ArrayList<>();
+        
+        dfs("ICN", "ICN", tickets, cnt);
+        
+        Collections.sort(allRoute);
+        answer = allRoute.get(0).split(" ");
+        
+        return answer;
+    }
+    
+    public void dfs(String start, String route, String[][] tickets, int cnt){
+        if(cnt == tickets.length){
+            allRoute.add(route);
+            return;
+        }
+        
+        for(int i=0; i<tickets.length; i++){
+            if(start.equals(tickets[i][0]) && !visited[i]){
+                visited[i] = true;
+                dfs(tickets[i][1], route+" "+tickets[i][1], tickets, cnt+1);
+                visited[i] = false;
+            }
+        }
+    }
 }
