949a574b149a035ddbabd110ac5fe77d56637fc3
DaftenP <80443975+DaftenP@users.noreply.github.com>
2024-04-18 17:54:50 +0900

    feat: BOJ 19236 (WA)

--- jiyong/Python/Week12/BOJ19236/BOJ_19236.py
+++ jiyong/Python/Week12/BOJ19236/BOJ_19236.py
@@ -1,11 +1,12 @@
 import sys
+from copy import deepcopy
 
 input = sys.stdin.readline
 
-delta = ((-1, 0), (-1, -1), (0, -1), (1, -1), (1, 0), (1, 1), (0, 1), (-1, 1))
+delta = ((-1, 1), (-1, 0), (-1, -1), (0, -1), (1, -1), (1, 0), (1, 1), (0, 1))
 
 
-def isIn(r: int, c: int) -> bool:
+def is_in(r: int, c: int) -> bool:
     return 0 <= r < 4 and 0 <= c < 4
 
 
@@ -16,13 +17,10 @@ class Shark:
         self.cnt = cnt
         self.d = d
 
-    def eat(self, r: int, c: int, map__: list):
-        pass
-
     def can_go(self) -> bool:
         nr = self.r + delta[self.d][0]
         nc = self.c + delta[self.d][1]
-        return True if isIn(nr, nc) else False
+        return True if is_in(nr, nc) else False
 
 
 class Fish:
@@ -32,29 +30,66 @@ class Fish:
         self.num = num
         self.d = d
 
-    def swap(self, fish):
-        self.r, fish.r = fish.r, self.r
-        self.c, fish.c = fish.c, self.c
+    def swap(self, map__: list, fish: list) -> None:
+        self.find_direction(map__)
+        nr, nc = self.r + delta[self.d][0], self.c + delta[self.d][1]
+        n = map__[nr][nc]
+        map__[self.r][self.c], map__[nr][nc] = map__[nr][nc], map__[self.r][self.c]
+        if n:
+            self.r, fish[n].r = fish[n].r, self.r
+            self.c, fish[n].c = fish[n].c, self.c
 
-    def find_direction(self, map__: list):
+        else:
+            self.r = nr
+            self.c = nc
+
+    def find_direction(self, map__: list) -> None:
         global delta
-        while not (0 <= self.r + delta[self.d][0] < 4 and 0 <= self.c + delta[self.d][1] < 4) or type(
-                map__[self.r + delta[self.d][0]][self.c + delta[self.d][1]]) == type(Shark):
-            d = (d + 1) % 8
+        while (not (0 <= self.r + delta[self.d][0] < 4 and 0 <= self.c + delta[self.d][1] < 4)) or \
+                map__[self.r + delta[self.d][0]][self.c + delta[self.d][1]] == -1:
+            self.d = (self.d + 1) % 8
 
 
-map_ = [[None] * 4 for _ in range(4)]
-fish_list = []
+map_ = [[0] * 4 for _ in range(4)]
+fish_list = [Fish(0, 0, 0, 0)]
 
 for r in range(4):
-    line = map(int, input().split())
+    line = list(map(int, input().split()))
     for c in range(4):
-        map_[r][c] = Fish(r, c, line[c * 2], line[c * 2 + 1])
-        fish_list.append(map_[r][c])
+        map_[r][c] = line[c * 2]
+        fish_list.append(Fish(r, c, line[c * 2], line[c * 2 + 1] % 8))
 
 fish_list.sort(key=lambda x: x.num)
 
 # 상어 생성
-fish_list.remove(map_[0][0])
-map_[0][0] = Shark(0, 0, map_[0][0].num, map_[0][0].d)
+shark = Shark(0, 0, fish_list[map_[0][0]].num, fish_list[map_[0][0]].d)
+fish_list[map_[0][0]] = None
+map_[0][0] = -1
 
+answer = 0
+
+
+def dfs(s: Shark, m: list, f: list):
+    global answer
+    for i in range(1, 17):
+        if f[i]:
+            f[i].swap(m, f)
+    if not s.can_go():
+        answer = max(answer, s.cnt)
+        return
+    else:
+        for i in range(1, 4):
+            nr, nc = s.r + i * delta[s.d][0], s.c + i * delta[s.d][1]
+            if is_in(nr, nc) and m[nr][nc] != 0:
+                nm = [x[:] for x in m]
+                nf = deepcopy(f)
+                ns = Shark(nr, nc, s.cnt + nf[nm[nr][nc]].num, nf[nm[nr][nc]].d)
+                nf[nm[nr][nc]] = None
+                nm[s.r][s.c] = 0
+                nm[nr][nc] = -1
+                dfs(ns, nm, nf)
+
+
+dfs(shark, map_, fish_list)
+
+print(answer)
