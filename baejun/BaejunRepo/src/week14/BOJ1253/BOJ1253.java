a81a85af29fb419c65a6d1358dc80ed453221e9c
sonbaejun <sonqowns2@naver.com>
2024-04-30 17:17:44 +0900

    feat : [공통][골드4] BOJ1253 좋다

--- baejun/BaejunRepo/src/week14/BOJ1253/BOJ1253.java
+++ baejun/BaejunRepo/src/week14/BOJ1253/BOJ1253.java
@@ -13,69 +13,64 @@ public class BOJ1253 {
 	static int N;
 	static long[] map;
 	static int answer;
-	
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		
+
 		N = Integer.parseInt(in.readLine());
 		map = new long[N];
-		boolean zeroExist = false;
-		
+
 		StringTokenizer st = new StringTokenizer(in.readLine());
 		for(int i = 0; i < N; i++) {
 			map[i] = Long.parseLong(st.nextToken());
-			if(map[i] == 0) zeroExist  = true;
 		}
-		
+
 		Arrays.sort(map);
-		
+
 		answer = 0;
 		for(int i = 0; i < N; i++) {
-			for(int j = 0; j < i; j++) {
-				boolean flag = binarySearch(j+1, N-1, map[i], map[j], i);
-				if(flag) {
-					answer++;
-					break;
-				}
-			}
-		}
-//		System.out.println(set.size());
-		System.out.println(answer);
-	}
-
-	private static boolean binarySearch(int left, int right, long target, long sumValue, int targetIdx) {
-		
-		while(left <= right) {
-			int mid = (left+right) / 2;
-			
-			if(mid != targetIdx) {
-				if(map[mid]+sumValue == target) {
-					return true;
-				}
-				
-				if(map[mid] + sumValue < target) {
-					left = mid + 1;
-				} else {
-					right = mid - 1;
-				}
-			} else {
-				if(map[mid] + sumValue == target) {
-					if((mid>0 && map[mid-1]==map[mid]) || (mid<N-1 && map[mid+1] == map[mid])) {
-						return true;
-					} else {
+			long val1 = map[i];
+			boolean flag = false;
+			for(int j = 0; j < N; j++) {
+				if(i == j) continue;
+				int left = 0;
+				int right = N-1;
+				while(left <= right) {
+					int mid = (left+right) / 2;
+					long target = val1 - map[j];
+					if(map[mid] == target && mid != i && mid != j) {
+						flag = true;
 						break;
 					}
-				} else {
-					if(map[mid] + sumValue < target) {
+
+					if(map[mid] < target) {
 						left = mid + 1;
 					} else {
 						right = mid - 1;
 					}
 				}
 			}
-			
+			if(flag) answer++;
 		}
-		
+		System.out.println(answer);
+	}
+
+	private static boolean binarySearch(int left, int right, long target, long sumValue, int targetIdx) {
+
+		while(left <= right) {
+			int mid = (left+right) / 2;
+
+			if(map[mid]+sumValue == target) {
+				return true;
+			}
+
+			if(map[mid] + sumValue < target) {
+				left = mid + 1;
+			} else {
+				right = mid - 1;
+			}
+		}
+
 		return false;
 	}
 
--- baejun/BaejunRepo/src/week14/BOJ1253/README.md
+++ baejun/BaejunRepo/src/week14/BOJ1253/README.md
@@ -1,58 +1,60 @@
 <h3 align="center"> 
-    📢  [골드4] 백준(불!) : https://www.acmicpc.net/problem/4179
+    📢  [골드4] 백준(좋다) : https://www.acmicpc.net/problem/1253
 </h3>
 
 <br>
 
 ## 🚀 문제
 
-지훈이는 미로에서 일을 한다. 지훈이를 미로에서 탈출하도록 도와주자!
-미로에서의 지훈이의 위치와 불이 붙은 위치를 감안해서 지훈이가 불에 타기전에 탈출할 수 있는지의 여부, 그리고 얼마나 빨리 탈출할 수 있는지를 결정해야한다.
-지훈이와 불은 매 분마다 한칸씩 수평또는 수직으로(비스듬하게 이동하지 않는다) 이동한다.
-불은 각 지점에서 네 방향으로 확산된다.
-지훈이는 미로의 가장자리에 접한 공간에서 탈출할 수 있다.
-지훈이와 불은 벽이 있는 공간은 통과하지 못한다.
+N개의 수 중에서 어떤 수가 다른 수 두 개의 합으로 나타낼 수 있다면 그 수를 “좋다(GOOD)”고 한다.
+N개의 수가 주어지면 그 중에서 좋은 수의 개수는 몇 개인지 출력하라.
+수의 위치가 다르면 값이 같아도 다른 수이다.
 
 ---
 
 ## 🚦입출력 + 제한사항
 
+- 첫째 줄에는 수의 개수 N(1 ≤ N ≤ 2,000), 두 번째 줄에는 i번째 수를 나타내는 Ai가 N개 주어진다. (|Ai| ≤ 1,000,000,000, Ai는 정수)
+- 좋은 수의 개수를 첫 번째 줄에 출력한다.
+
 ---
 
 ### 📜 문제 풀이(기능 목록, 접근법)
 **🕸접근법**
-- BFS 두번 돌림
-- 하나는 불이 퍼지는 시간을 기록하기 위한 BFS
-- 그리고 지훈이의 탈출 BFS, 이때 불이 퍼진 시간보다 일찍 가야 갈 수 있다는 조건 추가
+- 각 인덱스 순회
+- 안에서 또 인덱스를 순회하며 해당 인덱스 하나와 조건을 만족하는 target이 있는지를 이분탐색
 
-- [x] BFS
+- [x] 이분탐색
 
 ### 💻코드
 
 ```java
-private static void bfsInjihoon() {
-	Queue<Node> queue = new LinkedList<>();
-	queue.offer(new Node(startX, startY, 0));
-	visited[startX][startY] = true;
-	
-	while(!queue.isEmpty()) {
-		Node curNode = queue.poll();
-		for(int i = 0; i < 4; i++) {
-			int nr = curNode.x + dx[i];
-			int nc = curNode.y + dy[i];
-			
-			if(nr>=0&&nr<R&&nc>=0&&nc<C&&!visited[nr][nc]&&map[nr][nc]!='#'&&curNode.cost+1<fireMap[nr][nc]) {
-				visited[nr][nc] = true;
-				queue.offer(new Node(nr, nc, curNode.cost+1));
-				if(nr==0||nr==R-1||nc==0||nc==C-1) {
-					if(minValue > curNode.cost+1) minValue = curNode.cost+1;
-				}
+for(int i = 0; i < N; i++) {
+	long val1 = map[i];
+	boolean flag = false;
+	for(int j = 0; j < N; j++) {
+		if(i == j) continue;
+		int left = 0;
+		int right = N-1;
+		while(left <= right) {
+			int mid = (left+right) / 2;
+			long target = val1 - map[j];
+			if(map[mid] == target && mid != i && mid != j) {
+				flag = true;
+				break;
+			}
+
+			if(map[mid] < target) {
+				left = mid + 1;
+			} else {
+				right = mid - 1;
 			}
 		}
 	}
+	if(flag) answer++;
 }
 ```
 
 ### 🙄 후기
-소요시간 : 30분  <br>
-금방 로직 생각하고 풀었는데, 엣지를 못잡았다.. 이거 연습해야된다..
+소요시간 : 2시간 <br>
+개같다..
